93d88941399ff41f9dac63d9741e8274f5226dd8
Stuart J Nelson <stuart@northernbadger.co.uk>
2017-07-25 00:01:36 +0100

    ADde lastes bable package

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: ['es2015'],
+                        // npm install --save-dev babel-plugin-transform-es3-member-expression-literals
                         plugins: [
                             'transform-object-assign',
                             'syntax-dynamic-import'
