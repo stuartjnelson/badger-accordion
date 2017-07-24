fa2dce3151f26a023386b6f3e0973e48dec566a3
Stuart J Nelson <stuart@northernbadger.co.uk>
2017-07-24 23:19:35 +0100

    Creted behaviour to call accordion

---
+++ src/js/behaviour.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Importing accordion
+import BadgerAccordion from '../js/badger-accordion';
+
+// Creating a new instance of the accordion
+const accordion = new BadgerAccordion('.js-badger-accordion');
+
+// API Examples
+accordion.open( document.querySelector('[data-badger-accordion-header-id="1"]') );
+// accordion.openAll();
