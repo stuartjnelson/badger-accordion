15083cbee88718d638f2732a66ce2a4b32cbbf15
Stuart Nelson <stuart@northernbadger.co.uk>
2017-12-04 17:50:53 -0800

    Fixed rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,28 @@
 import babel from 'rollup-plugin-babel';
 import eslint from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import includePaths from 'rollup-plugin-includepaths';
+// import uglify from 'rollup-plugin-uglify';
 
 export default {
-    entry     : 'src/js/badger-accordion.js',
-    dest      : 'dist/badger-accordion.min.js',
-    format    : 'umd',
-    sourceMap : 'inline',
-    moduleName: 'BadgerAccordion',
-    external  : ['uuid/v4', 'transition-end'],
-    globals   : {
-        'uuid/v4'        : 'uuidV4',
-        'transition-end' : 'onCSSTransitionEnd'
+    // input: 'src/js/badger-accordion.js',
+    input: 'example/js/behaviour.js',
+    sourcemap: 'inline',
+    name: 'BadgerAccordion',
+    output: {
+        // file: 'dist/badger-accordion.js',
+        file: 'example/js/app.js',
+        format: 'umd',
     },
     plugins: [
+        resolve(),
+        commonjs(),
         eslint({
-            exclude: [
-                'src/css/**',
-                'src/scss/**'
-            ]
+            exclude: ['src/css/**', 'src/scss/**']
         }),
-        babel({
-            exclude: 'node_modules/**',
-        }),
-    ],
+        babel({exclude: 'node_modules/**'}),
+        includePaths(),
+        // uglify()
+    ]
 };
