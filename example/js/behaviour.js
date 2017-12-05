421382d552f457cb668a8579ef33995ceb5578e4
Stuart Nelson <stuart@northernbadger.co.uk>
2017-12-04 17:53:06 -0800

    Disbaling some ESlint errors in example js

--- example/js/behaviour.js
+++ example/js/behaviour.js
@@ -1,13 +1,15 @@
 'use strict';
 
 // Importing accordion
-import pollyfill from 'array-from-pollyfill';
-import BadgerAccordion from 'badger-accordion';
+/* eslint-disable no-unused-vars */
+import pollyfill from 'dist/array-from-pollyfill';
+import BadgerAccordion from 'dist/badger-accordion';
 
 // Creating a new instance of the accordion
 const accordion = new BadgerAccordion('.js-badger-accordion');
 
 // API Examples
+/* eslint-disable no-console */
 console.log(accordion.getState( [0] ));
 // accordion.open( document.querySelector('[data-badger-accordion-header-id="1"]') );
 // accordion.close( 0 );
